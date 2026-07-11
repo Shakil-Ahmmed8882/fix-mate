eaa3c7af7f1cce5704b1e1c588cb8802ffb42f28
Shakil-Ahmmed8882 <shakilahmmed8882@gmail.com>
2026-07-11 19:36:39 +0600

    feat(utils): add errorDetails support to AppError

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,9 +1,11 @@
 export class AppError extends Error {
     statusCode: number;
+    errorDetails: unknown;
 
-    constructor(statusCode: number, message: string) {
+    constructor(statusCode: number, message: string, errorDetails: unknown = null) {
         super(message);
         this.statusCode = statusCode;
+        this.errorDetails = errorDetails;
         Error.captureStackTrace(this, this.constructor);
     }
 }
