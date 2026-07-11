cc43aab652862137686e697099075c4dfa441b70
Shakil-Ahmmed8882 <shakilahmmed8882@gmail.com>
2026-07-11 17:04:05 +0600

    feat(user): add user controllers

---
+++ src/modules/user/users.controller.ts
@@ -0,0 +1,56 @@
+import type { Request, Response } from "express";
+import httpStatus from "http-status";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { UserServices } from "./users.services";
+
+const registerUser = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserServices.registerUserIntoDB(req.body);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User registered successfully",
+        data: { user },
+    });
+});
+
+const getMyProfile = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserServices.getMyProfileFromDB(req.user!.email);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Profile retrieved successfully",
+        data: { user },
+    });
+});
+
+const updateMyProfile = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserServices.updateMyProfileIntoDB(req.user!.email, req.body);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Profile updated successfully",
+        data: { user },
+    });
+});
+
+const updateMyPassword = catchAsync(async (req: Request, res: Response) => {
+    await UserServices.updateMyPasswordIntoDB(req.user!.email, req.body);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Password updated successfully",
+        data: null,
+    });
+});
+
+export const userController = {
+    registerUser,
+    getMyProfile,
+    updateMyProfile,
+    updateMyPassword,
+};
